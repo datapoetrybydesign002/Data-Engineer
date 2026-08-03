1d7bd6b9ba6dae291c4498b2a8184ba5d4ed09fb
datapoetrybydesign002 <datapoetrybydesign_002@outlook.com>
2026-08-03 12:52:36 -0400

    Hugo - Added new tables to EarlyON DDL file

--- EarlyON_DDL.sql
+++ EarlyON_DDL.sql
@@ -41,18 +41,122 @@ BEGIN
         
         -- PRIMARY KEY constraint defined at the end
         CONSTRAINT PK_Province_ID PRIMARY KEY CLUSTERED ([ID] ASC)
-    );   
-    
+    );    
 END
 
 
 -- 2)  dbo.ServiceManager - Wyman
+
+
+
 -- 3)  dbo.CityManager - Hugo
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CityManager]') AND type in (N'U'))
+BEGIN
+    DROP TABLE [dbo].[CityManager];    
+END
+
+BEGIN
+    CREATE TABLE [dbo].[CityManager] (
+        -- Primary key with IDENTITY (auto-increment by 1)
+        [ID] INT IDENTITY(1,1) NOT NULL,
+        [ServiceManagerID] INT NULL,
+        [CityID] INT NULL,
+
+        -- Audit columns with SQL Server predefined values
+        [CreatedDateTime] DATETIME2 NOT NULL DEFAULT GETDATE(),
+        [CreatedBy] VARCHAR(300) NOT NULL DEFAULT SUSER_SNAME(),
+        [ModifiedDateTime] DATETIME2 NOT NULL DEFAULT GETDATE(),
+        [ModifiedBy] VARCHAR(128) NOT NULL DEFAULT SUSER_SNAME(),
+        
+        -- PRIMARY KEY constraint defined at the end
+        CONSTRAINT PK_City_Manager_ID PRIMARY KEY CLUSTERED ([ID] ASC)
+    );    
+END
+
+
+
+
 -- 4)  dbo.City - Wyman
+
+
 -- 5)  dbo.Municipality - Hugo
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Municipality]') AND type in (N'U'))
+BEGIN
+    DROP TABLE [dbo].[Municipality];    
+END
+
+BEGIN
+    CREATE TABLE [dbo].[Municipality] (
+        -- Primary key with IDENTITY (auto-increment by 1)
+        [ID] INT IDENTITY(1,1) NOT NULL,
+        [Municipality] VARCHAR(300) NULL,       
+
+        -- Audit columns with SQL Server predefined values
+        [CreatedDateTime] DATETIME2 NOT NULL DEFAULT GETDATE(),
+        [CreatedBy] VARCHAR(300) NOT NULL DEFAULT SUSER_SNAME(),
+        [ModifiedDateTime] DATETIME2 NOT NULL DEFAULT GETDATE(),
+        [ModifiedBy] VARCHAR(128) NOT NULL DEFAULT SUSER_SNAME(),
+        
+        -- PRIMARY KEY constraint defined at the end
+        CONSTRAINT PK_Municipality_ID PRIMARY KEY CLUSTERED ([ID] ASC)
+    );    
+END
+
+
 -- 6)  dbo.CityLocation - Wyman
+
+
 -- 7)  dbo.Location - Hugo
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Location]') AND type in (N'U'))
+BEGIN
+    DROP TABLE [dbo].[Location];    
+END
+
+BEGIN
+    CREATE TABLE [dbo].[Location] (
+        -- Primary key with IDENTITY (auto-increment by 1)
+        [ID] INT IDENTITY(1,1) NOT NULL,
+        [Location] VARCHAR(300) NULL,       
+
+        -- Audit columns with SQL Server predefined values
+        [CreatedDateTime] DATETIME2 NOT NULL DEFAULT GETDATE(),
+        [CreatedBy] VARCHAR(300) NOT NULL DEFAULT SUSER_SNAME(),
+        [ModifiedDateTime] DATETIME2 NOT NULL DEFAULT GETDATE(),
+        [ModifiedBy] VARCHAR(128) NOT NULL DEFAULT SUSER_SNAME(),
+        
+        -- PRIMARY KEY constraint defined at the end
+        CONSTRAINT PK_Location_ID PRIMARY KEY CLUSTERED ([ID] ASC)
+    );    
+END
+
+
 -- 8)  dbo.LocationAddress - Wyman
+
+
 -- 9)  dbo.Address - Hugo
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Address]') AND type in (N'U'))
+BEGIN
+    DROP TABLE [dbo].[Address];    
+END
+
+BEGIN
+    CREATE TABLE [dbo].[Address] (
+        -- Primary key with IDENTITY (auto-increment by 1)
+        [ID] INT IDENTITY(1,1) NOT NULL,
+        [Address] VARCHAR(300) NULL,       
+
+        -- Audit columns with SQL Server predefined values
+        [CreatedDateTime] DATETIME2 NOT NULL DEFAULT GETDATE(),
+        [CreatedBy] VARCHAR(300) NOT NULL DEFAULT SUSER_SNAME(),
+        [ModifiedDateTime] DATETIME2 NOT NULL DEFAULT GETDATE(),
+        [ModifiedBy] VARCHAR(128) NOT NULL DEFAULT SUSER_SNAME(),
+        
+        -- PRIMARY KEY constraint defined at the end
+        CONSTRAINT PK_Address_ID PRIMARY KEY CLUSTERED ([ID] ASC)
+    );    
+END
+
 -- 10) dbo.AddressPostalCode - Wyman
+
+
 -- 11) dbo.PostalCode - Hugo & Wyman
